b0e64a3f61f8a2f2f429063e723e90910917dc6c
sergemr <sergemr@gmail.com>
2023-11-02 16:08:27 -0600

    initial commit

---
+++ semana8/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/home/home";
+import List from "./components/list/list";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App">
+        Hello
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/list" element={<List />} />
+          </Routes>
+        </Router>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ semana8/src/components/list/list.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './list.module.css';
+
+const List = () => (
+  <div className={styles.List}>
+    List Component
+  </div>
+);
+
+List.propTypes = {};
+
+List.defaultProps = {};
+
+export default List;
