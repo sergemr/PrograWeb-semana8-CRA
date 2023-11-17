6df3a4e645196daee451f948f285aadc31eb0444
Sergio Morales <sergemr@gmail.com>
2023-11-16 20:17:58 -0600

    Update Note.js

--- semana8/src/components/Note/Note.js
+++ semana8/src/components/Note/Note.js
@@ -1,13 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Note.css";
+import TextField from "@mui/material/TextField";
 
+import Button from "@mui/material/Button";
 const Note = (props) => (
   <div className="Note" data-testid="Note">
     {console.log("props", props)}
-    <h2>{props.titulo}</h2>
-    <h1>{props.note.Title}</h1>
-    <p>{props.note.Content} </p>
+
+    <TextField
+      id="outlined-basic"
+      name="usuario"
+      // label="Outlined"
+      defaultValue={props.note.Title}
+      // onChange={onChancheInput}
+      variant="standard"
+    />
+    <TextField
+      id="outlined-basic"
+      name="usuario"
+      // label="Outlined"
+      defaultValue={props.note.Content}
+      // onChange={onChancheInput}
+      variant="standard"
+    />
+    <br />
+    <Button color="secondary" variant="text">
+      editar
+    </Button>
   </div>
 );
 
